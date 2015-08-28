081017c12a228118c1cade7a9d054b55d408baf5
Juan M. Ordaz Velazquez <juanordaz_2011@icloud.com>
2015-08-28 12:04:21 -0400

    Lista de actividades now are the root of the page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :lista_de_actividades
+  root 'lista_de_actividades#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
